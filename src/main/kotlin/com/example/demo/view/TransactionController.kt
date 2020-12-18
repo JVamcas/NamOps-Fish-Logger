a07d4a9a333d0dbde21748710952c77176b34687
Petrus Kambala <kmbpet001@myuct.ac.za>
2020-12-18 16:44:54 +0200

    initial commit

---
+++ src/main/kotlin/com/example/demo/view/TransactionController.kt
@@ -0,0 +1,8 @@
+package com.example.demo.view
+
+import javafx.scene.layout.BorderPane
+import tornadofx.*
+
+class TransactionController : View("My View") {
+    override val root: BorderPane by fxml()
+}
